43e2f195003ae02a713655e10d0bb01de26514c1
Tyler Yannes <tyleryannes@Tylers-MacBook-Pro.local>
2023-12-25 10:34:28 -0500

    Finished the seeds.sql file.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department (name) VALUES ('Sales'), ('Engineer'),('Finance'), ('Legal');
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', 100000,1), ('Software Engineer',120000, 2);
+INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ('Tyler','Yannes',1, NULL)
