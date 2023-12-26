67ded3c5ff7d54f321710e159dff921d50c9e072
Tyler Yannes <tyleryannes@Tylers-MBP.home>
2023-12-26 17:15:24 -0600

    Updated the seeds.sql file to include more examples.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,19 @@
-INSERT INTO department (name) VALUES ('Sales'), ('Engineer'),('Finance'), ('Legal');
-INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', 100000,1), ('Software Engineer',120000, 2);
-INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ('Tyler','Yannes',1, NULL)
+INSERT INTO department (name) VALUES 
+('Sales'), 
+('Engineering'),
+('Finance'), 
+('Legal');
+
+INSERT INTO role (title, salary, department_id) VALUES 
+('Sales Lead', 100000, 1), 
+('Software Engineer', 120000, 2),
+('Financial Analyst', 90000, 3),
+('Legal Advisor', 110000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('Tyler', 'Yannes', 1, NULL),
+('Jane', 'Doe', 2, 1),
+('John', 'Smith', 3, 1),
+('Alice', 'Johnson', 4, 2),
+('Bob', 'Brown', 1, 2),
+('Emma', 'Davis', 3, 2);
