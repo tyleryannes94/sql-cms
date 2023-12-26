d3d89d166bdd33255aa31110cd82c1dc13874541
Tyler Yannes <tyleryannes@Tylers-MBP.home>
2023-12-26 17:07:06 -0600

    Added more functions and added them as prompts via the startApp function.

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ function startApp() {
             'View all employees',
             'View all departments',
             'View all roles',
+            'Update employee manager',
+            'View employees by manager',
+            'View employees by department',
+            'Delete a department',
+            'View department budget',
             'Exit'
         ]
     })
@@ -38,12 +43,28 @@ function startApp() {
             case 'View all roles':
                 viewAllRoles();
                 break;
+            case 'Update employee manager':
+                updateEmployeeManager();
+                break;
+            case 'View employees by manager':
+                viewEmployeesByManager();
+                break;
+            case 'View employees by department':
+                viewEmployeesByDepartment();
+                break;
+            case 'Delete a department':
+                deleteDepartment();
+                break;
+            case 'View department budget':
+                viewDepartmentBudget();
+                break;
             case 'Exit':
                 connection.end();
                 break;
         }
     });
 }
+
 
 function viewAllEmployees() {
     connection.query('SELECT * FROM employee', (err, res) => {
@@ -66,5 +87,154 @@ function viewAllRoles() {
         if (err) throw err;
         console.log(res);
         startApp();
+    });
+}
+
+function updateEmployeeManager() {
+    connection.query('SELECT id, first_name, last_name FROM employee', (err, employees) => {
+        if (err) throw err;
+
+        const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'employeeId',
+                type: 'list',
+                message: 'Which employee\'s manager do you want to update?',
+                choices: employeeChoices
+            },
+            {
+                name: 'managerId',
+                type: 'list',
+                message: 'Who is the new manager?',
+                choices: employeeChoices
+            }
+        ])
+        .then(answers => {
+            connection.query('UPDATE employee SET manager_id = ? WHERE id = ?', [answers.managerId, answers.employeeId], err => {
+                if (err) throw err;
+                console.log('Updated employee\'s manager.');
+                startApp();
+            });
+        });
+    });
+}
+
+function viewEmployeesByManager() {
+    connection.query('SELECT id, first_name, last_name FROM employee WHERE manager_id IS NOT NULL', (err, managers) => {
+        if (err) throw err;
+
+        const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'managerId',
+                type: 'list',
+                message: 'Select a manager to view their employees:',
+                choices: managerChoices
+            }
+        ])
+        .then(answer => {
+            connection.query('SELECT first_name, last_name FROM employee WHERE manager_id = ?', answer.managerId, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                startApp();
+            });
+        });
+    });
+}
+
+function viewEmployeesByDepartment() {
+    connection.query('SELECT id, name FROM department', (err, departments) => {
+        if (err) throw err;
+
+        const departmentChoices = departments.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'departmentId',
+                type: 'list',
+                message: 'Select a department:',
+                choices: departmentChoices
+            }
+        ])
+        .then(answer => {
+            connection.query(
+                'SELECT employee.first_name, employee.last_name FROM employee JOIN role ON employee.role_id = role.id WHERE role.department_id = ?', 
+                answer.departmentId, 
+                (err, res) => {
+                    if (err) throw err;
+                    console.table(res);
+                    startApp();
+                }
+            );
+        });
+    });
+}
+
+function deleteDepartment() {
+    connection.query('SELECT id, name FROM department', (err, departments) => {
+        if (err) throw err;
+
+        const departmentChoices = departments.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'departmentId',
+                type: 'list',
+                message: 'Select a department to delete:',
+                choices: departmentChoices
+            }
+        ])
+        .then(answer => {
+            connection.query('DELETE FROM department WHERE id = ?', answer.departmentId, err => {
+                if (err) throw err;
+                console.log('Department deleted.');
+                startApp();
+            });
+        });
+    });
+}
+
+function viewDepartmentBudget() {
+    connection.query('SELECT id, name FROM department', (err, departments) => {
+        if (err) throw err;
+
+        const departmentChoices = departments.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'departmentId',
+                type: 'list',
+                message: 'Select a department to view its utilized budget:',
+                choices: departmentChoices
+            }
+        ])
+        .then(answer => {
+            connection.query(
+                'SELECT SUM(salary) AS utilized_budget FROM role JOIN employee ON role.id = employee.role_id WHERE department_id = ?', 
+                answer.departmentId, 
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`Total Utilized Budget: ${res[0].utilized_budget}`);
+                    startApp();
+                }
+            );
+        });
     });
 }
